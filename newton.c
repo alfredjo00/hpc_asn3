08bb67f616cf7e0d6931fc8f0ebdb537aa1c53ca
alfred <alfredjo00@gmail.com>
2023-10-17 17:31:40 +0200

    added check thread func

--- newton.c
+++ newton.c
@@ -4,51 +4,113 @@
 #include <assert.h>
 #include <threads.h>
 
+
 typedef struct {
-	const float **v;
-	float **w;
-	int ib;
-	int istep;
-	int sz;
-	int tx;
-	mtx_t *mtx;
+  int val;
+  char pad[60]; // cacheline - sizeof(int)
+} int_padded;
+
+typedef struct {
+  float **w;
+	int **f;
+  int ib;
+  int istep;
+  int sz;
+  int tx;
+  mtx_t *mtx;
+  cnd_t *cnd;
+  int_padded *status;
 } thrd_info_t;
+
+typedef struct {
+  float **w;
+	int **f;
+  int sz;
+  int nthrds;
+  mtx_t *mtx;
+  cnd_t *cnd;
+  int_padded *status;
+} thrd_info_check_t;
+
 
 int thrd_fun(void *args)
 {
-	const thrd_info_t *thrd_info = (thrd_info_t*) args;
-	const float **v = thrd_info->v;
-	float **w = thrd_info->w;
-	const int ib = thrd_info->ib;
-	const int istep = thrd_info->istep;
-	const int sz = thrd_info->sz;
-	const int tx = thrd_info->tx;
-	mtx_t *mtx = thrd_info->mtx;
+  const thrd_info_t *thrd_info = (thrd_info_t*) args;
+  float **w 					= thrd_info->w;
+  int **f 						= thrd_info->f;
+  const int ib 				= thrd_info->ib;
+  const int istep			= thrd_info->istep;
+  const int sz 				= thrd_info->sz;
+  const int tx 				= thrd_info->tx;
+  mtx_t *mtx 					= thrd_info->mtx;
+  cnd_t *cnd 					= thrd_info->cnd;
+  int_padded *status 	= thrd_info->status;
 	
 	for ( int ix = ib; ix < sz; ix += istep ) {
-		const float *vix = v[ix];
 		// We allocate the rows of the result before computing, and free them in another thread.
-		float *wix = (float*) malloc(3*sz*sizeof(float));
+		float *wix 	= (float*) malloc(3*sz*sizeof(float));
+		int *fix 		= (int*) 	malloc(sz*sizeof(int));
 
 		for ( int jx = 0; jx < sz; ++jx ){			
-			wix[3 * jx] 		= jx % 20; 	// R
+			wix[3 * jx] 			= jx % 20; 	// R
 			wix[3 * jx + 1] 	= jx % 100; // G
 			wix[3 * jx + 2] 	= jx % 200; // B
+			fix[jx] 					= jx % 20;
 		}
 
 		mtx_lock(mtx);
-		w[ix] = wix;
-		mtx_unlock(mtx);
+		w[ix] = wix; // array of RGB
+		f[ix] = fix; // array of iterations done
+    status[tx].val = ix + istep;
+    mtx_unlock(mtx);
+    cnd_signal(cnd);
 	}
 	printf("Done %d\n", tx);
 	return 0;
 }
 
-int write_ppm(float **w, int n_size)
-{	
-	
-	for (int i = 0; i < n_size; i++)
-		free(w[i]);
+
+int thrd_check_fun(void *args)
+{
+  const thrd_info_check_t *thrd_info = (thrd_info_check_t*) args;
+  float **w 					= thrd_info->w;
+  int **f 						= thrd_info->f;
+  const int sz 				= thrd_info->sz;
+  const int nthrds 		= thrd_info->nthrds;
+  mtx_t *mtx 					= thrd_info->mtx;
+  cnd_t *cnd 					= thrd_info->cnd;
+  int_padded *status 	= thrd_info->status;
+
+  // We do not increment ix in this loop, but in the inner one.
+  for ( int ix = 0, ibnd; ix < sz; ) {
+
+    // If no new lines are available, we wait.
+    for ( mtx_lock(mtx); ; ) {
+      // We extract the minimum of all status variables.
+      ibnd = sz;
+      for ( int tx = 0; tx < nthrds; ++tx )
+        if ( ibnd > status[tx].val )
+          ibnd = status[tx].val;
+
+      if ( ibnd <= ix )
+        // We rely on spurious wake-ups, which in practice happen, but are not
+        // guaranteed.
+        cnd_wait(cnd,mtx);
+      else {
+        mtx_unlock(mtx);
+        break;
+      }
+    }
+
+    // We do not initialize ix in this loop, but in the outer one.
+    for ( ; ix < ibnd; ++ix ) {
+      // We free the component of w, since it will never be used again.
+      free(w[ix]);
+      free(f[ix]);
+    }
+  }
+
+  return 0;
 }
 
 
@@ -66,53 +128,71 @@ int main(int argc, char *argv[]) {
 	assert(n_size != 0 && n_threads != 0);	
 	
 	float **w = (float**) malloc(n_size*sizeof(float*));
-	float **v = (float**) malloc(n_size*sizeof(float*));
-	float *ventries = (float*) malloc(2*n_size*n_size*sizeof(float));
+	int **f 	= (int**) malloc(n_size*sizeof(int*));
 
+  thrd_t thrds[n_threads];
+  thrd_info_t thrds_info[n_threads];
 
-	for ( int ix = 0, jx = 0; ix < n_size; ++ix, jx += 2 * n_size )
-		v[ix] = ventries + jx;
+  thrd_t thrd_check;
+  thrd_info_check_t thrd_info_check;
+  
+  mtx_t mtx;
+  mtx_init(&mtx, mtx_plain);
 
-	float delta = 4 / n_size;
-	for ( int ix = 0; ix < n_size; ++ix )
-	{
-		for ( int jx = 0; jx < n_size; ++jx )
-		{	
-			// Real and imaginary coords. (re, im)
-			ventries[2 * ix * n_size + 2 * jx] 		= -2 + ix * delta;
-			ventries[2 * ix * n_size + 2 * jx + 1] =  2 - jx * delta;
-		}
-	}
-	
-	thrd_t thrds[n_threads];
-	thrd_info_t thrds_info[n_threads];
+  cnd_t cnd;
+  cnd_init(&cnd);
 
-	mtx_t mtx;
-	mtx_init(&mtx, mtx_plain);
+  int_padded status[n_threads];
 
 	for ( int tx = 0; tx < n_threads; ++tx ) {
-		thrds_info[tx].v = (const float**) v;
-		thrds_info[tx].w = w;
-		thrds_info[tx].ib = tx;
-		thrds_info[tx].istep = n_threads;
-		thrds_info[tx].sz = n_size;
-		thrds_info[tx].tx = tx;
-		thrds_info[tx].mtx = &mtx;
+    thrds_info[tx].w 			= w;
+    thrds_info[tx].f 			= f;
+    thrds_info[tx].ib	 		= tx;
+    thrds_info[tx].istep 	= n_threads;
+    thrds_info[tx].sz 		= n_size;
+    thrds_info[tx].tx 		= tx;
+    thrds_info[tx].mtx 		= &mtx;
+    thrds_info[tx].cnd 		= &cnd;
+    thrds_info[tx].status = status;
+    status[tx].val 				= 0;
 
 		int r = thrd_create(thrds+tx, thrd_fun, (void*) (thrds_info+tx));
 		if ( r != thrd_success ) {
 			fprintf(stderr, "failed to create thread\n");
 			exit(1);
 		}
+    thrd_detach(thrds[tx]);
 	}	
 
-	for ( int tx = 0; tx < n_threads; ++tx ) {
-		int r;
-		thrd_join(thrds[tx], &r);
-	}
+  {
+    thrd_info_check.w 			= w;
+    thrd_info_check.f 			= f;
+    thrd_info_check.sz 			= n_size;
+    thrd_info_check.nthrds 	= n_threads;
+    thrd_info_check.mtx 		= &mtx;
+    thrd_info_check.cnd 		= &cnd;
+    // It is important that we have initialize status in the previous for-loop,
+    // since it will be consumed by the check threads.
+    thrd_info_check.status = status;
+
+    int r = thrd_create(&thrd_check, thrd_check_fun, (void*) (&thrd_info_check));
+    if ( r != thrd_success ) {
+      fprintf(stderr, "failed to create thread\n");
+      exit(1);
+    }
+  }
+
+  {
+    int r;
+    thrd_join(thrd_check, &r);
+  }
 
 	// Write to ppm files
 
-	mtx_destroy(&mtx);
-		return 0;
+  free(w);
+
+  mtx_destroy(&mtx);
+  cnd_destroy(&cnd);
+	
+	return 0;
 }
